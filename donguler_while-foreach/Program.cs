f37652ce5ed9f76cb542e9876d3f584e6b1ad930
Bilal Günaydın <bilallgunaydin@gmail.com>
2021-09-17 23:27:36 +0300

    Add files via upload

---
+++ donguler_while-foreach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            //1'den başlayarak console'dan girilen sayıya kadar ( sayı dahil) ortalama hesaplayıp consol'a yazdıran program.
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi=int.Parse(Console.ReadLine());
+            int sayac=1;
+            int toplam=0;
+            while(sayac<=sayi){
+                toplam +=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            // "a"'dan "z"'ye kadar olan tüm harfleri console'a yazdır.
+
+            char character= 'a';
+            while(character<='z'){
+                Console.Write(character);
+                character++;
+            }
+
+            //Foreach
+            string [] arabalar={"bmw","mercedes"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
