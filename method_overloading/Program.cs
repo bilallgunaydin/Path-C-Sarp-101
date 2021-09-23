d1dbba6af97d45c4b1d32fe60ff6f8db4574dec6
Bilal Günaydın <bilallgunaydin@gmail.com>
2021-09-23 19:00:50 +0300

    Add files via upload

---
+++ method_overloading/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace method_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // out parametreler
+            string sayi="999";
+            int outSayi;
+            bool sonuc= int.TryParse(sayi, out outSayi);
+            if(sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+                Console.WriteLine("Başarısız");
+
+                Metotlar instance= new Metotlar();
+                instance.Topla(4,3, out int toplamsunucu);
+                Console.WriteLine(toplamsunucu);
+
+            // Metot Aşırı Yükleme - Overloading
+
+            int ifade= 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Günaydın", "Arkadaşlar");
+
+            // Metot İmzası
+            // MetotAdı + parametre sayısı + parametre
+        }
+    }
+
+    class Metotlar
+    {
+            public void Topla(int a, int b, out int toplam)
+            {
+                toplam=a+b;
+            }
+
+            public void EkranaYazdir(string veri){
+
+                Console.WriteLine(veri);
+            }
+
+            public void EkranaYazdir(int veri){
+
+                Console.WriteLine(veri);
+            }
+
+            public void EkranaYazdir(string veri1, string veri2)
+            {
+
+                Console.WriteLine(veri1+veri2);
+            }
+    }
+}
