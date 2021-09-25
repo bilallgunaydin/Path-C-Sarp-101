9384269f63d2cff4ec27d03c3f9a43ddc280e391
Bilal Günaydın <bilallgunaydin@gmail.com>
2021-09-25 23:15:30 +0300

    Add files via upload

---
+++ stringMethods/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace stringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string degisken ="Dersimiz Charp, Hoşgeldiniz!";
+          string degisken2 =" Charp";
+          //Length;
+          Console.WriteLine(degisken.Length);
+
+          //ToUpper, ToLower
+          Console.WriteLine(degisken.ToUpper());
+          Console.WriteLine(degisken.ToLower());
+
+          //Concat
+
+          Console.WriteLine(String.Concat(degisken, " Merhaba!"));
+
+          //Compare, CompareTo
+          Console.WriteLine(degisken.CompareTo(degisken2));  //0,1,-1
+          Console.WriteLine(String.Compare(degisken,degisken2, true)); //0,1,-1
+          Console.WriteLine(String.Compare(degisken,degisken2, false));   
+
+          //Contains
+          Console.WriteLine(degisken.Contains(degisken2));
+          Console.WriteLine(degisken.EndsWith("Hoşgeldiniz"));
+          Console.WriteLine(degisken.StartsWith("Merhaba!"));
+
+          //Indexof
+
+          Console.WriteLine(degisken.IndexOf("CS"));
+          Console.WriteLine(degisken.IndexOf("Bilal"));
+
+          //LastIndexof
+
+          Console.WriteLine(degisken.LastIndexOf("i"));
+         
+          //Insert
+          Console.WriteLine(degisken.Insert(0, "Merhaba! "));
+
+          //Padleft, PadRight
+
+          Console.WriteLine(degisken +degisken.PadLeft(30));
+          Console.WriteLine(degisken +degisken.PadRight(30, '*'));
+          Console.WriteLine(degisken.PadRight(50) + degisken2);
+          Console.WriteLine(degisken.PadRight(50, '-') + degisken2);
+
+          //Remove
+
+          Console.WriteLine(degisken.Remove(10));
+          Console.WriteLine(degisken.Remove(5,3));
+          Console.WriteLine(degisken.Remove(0,1));
+
+          //Replace
+
+          Console.WriteLine(degisken.Replace("CSharp","C#"));
+          Console.WriteLine(degisken.Replace(" ","*"));
+
+          //Split
+          Console.WriteLine(degisken.Split(' ')[1]);
+
+          //Substring
+
+          Console.WriteLine(degisken.Substring(4));
+          Console.WriteLine(degisken.Substring(4,6));
+        }
+    }
+}
