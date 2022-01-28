0c929b89d50c9dda35330a696a9a112d368dde69
Bilal Günaydın <bilallgunaydin@gmail.com>
2022-01-28 22:21:04 +0300

    Add files via upload

---
+++ BookStore2/WebApi/BookOperations/DeleteBook/DeleteBookCommandValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+
+namespace WebApi.BookOperations.DeleteBook
+{
+    public class DeleteBookCommandValidator : AbstractValidator<DeleteBookCommand>
+    {
+        public DeleteBookCommandValidator()
+        {
+            RuleFor(command => command.BookId).GreaterThan(0);
+
+        }
+    }
+    
+}
